129f1db43c7690ed242a3af6f467ae20b4d24cfa
AlexanderKamynin <kamyninsasa@gmail.com>
2024-11-11 16:09:27 +0300

    update: backend login routes update
    Co-authored-by: Усачева Дарья <Dariiiii@users.noreply.github.com>

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,5 +1,5 @@
-from .auth_routes import auth_bp
-from .request_routes import credit_request_bp
+# from .auth_routes import auth_bp
+# from .request_routes import credit_request_bp
 
 # Создание списка всех маршрутов для удобного подключения к основному приложению
-__all__ = ["auth_bp", "request_bp"]
+# __all__ = ["auth_bp", "request_bp"]
--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -1,28 +1,74 @@
 from flask import Blueprint, request, jsonify
 from models import Client, Admin
 from werkzeug.security import check_password_hash
+import jwt
 import mongoengine
 
 bp = Blueprint('auth_routes', __name__)
 
-@bp.route('/login/client', methods=['POST'])
-def client_login():
+# @bp.route('/login/client', methods=['POST'])
+# def client_login():
+#     data = request.get_json()
+#     email = data.get('email')
+#     password = data.get('password')
+
+#     client = Client.objects(email=email).first()
+#     if client and check_password_hash(client.password, password):
+#         return jsonify({"message": "Client logged in successfully", "client_id": str(client.id)}), 200
+#     return jsonify({"error": "Invalid email or password"}), 401
+
+# @bp.route('/login/admin', methods=['POST'])
+# def admin_login():
+#     data = request.get_json()
+#     email = data.get('email')
+#     password = data.get('password')
+
+#     admin = Admin.objects(email=email).first()
+#     if admin and check_password_hash(admin.password, password):
+#         return jsonify({"message": "Admin logged in successfully", "admin_id": str(admin.id)}), 200
+#     return jsonify({"error": "Invalid email or password"}), 401
+
+def generate_token(user_id):
+    token = jwt.encode({
+        'user_id': user_id,
+    }, key=user_id, algorithm='HS256')
+    return token
+
+@bp.route('/login', methods=['POST'])
+def login():
     data = request.get_json()
     email = data.get('email')
     password = data.get('password')
-
-    client = Client.objects(email=email).first()
-    if client and check_password_hash(client.password, password):
-        return jsonify({"message": "Client logged in successfully", "client_id": str(client.id)}), 200
-    return jsonify({"error": "Invalid email or password"}), 401
-
-@bp.route('/login/admin', methods=['POST'])
-def admin_login():
-    data = request.get_json()
-    email = data.get('email')
-    password = data.get('password')
-
+    
+    print("Попытка входа со следующими данными:", data)
+    
+    # TODO: нужно исправить на сверку данных с БД
     admin = Admin.objects(email=email).first()
+    client = Client.objects(email=email).first()
+    
+    # TODO: исправить хардкод почты
+    if email == "admin@example.com":
+        token = generate_token('1')
+        return jsonify({"message": "Admin logged in successfully",
+                        "admin_id": str(1),
+                        "token": token}), 200
+    if email == "client@example.com":
+        token = generate_token('1')
+        return jsonify({"message": "Client logged in successfully",
+                        "admin_id": str(1),
+                        "token": token}), 200
+    
     if admin and check_password_hash(admin.password, password):
-        return jsonify({"message": "Admin logged in successfully", "admin_id": str(admin.id)}), 200
+        token = generate_token(str(admin.id))
+        return jsonify({"message": "Admin logged in successfully",
+                        "admin_id": str(admin.id),
+                        "token": token}), 200
+        
+    if client and check_password_hash(client.password, password):
+        token = generate_token(str(client.id))
+        return jsonify({"message": "Admin logged in successfully",
+                        "admin_id": str(client.id),
+                        "token": token}), 200
+        
     return jsonify({"error": "Invalid email or password"}), 401
+
